73bb16aec6e5c793d316194e86fe1e98b72b4ba5
RyaWcksn <pram.aryawcksn@protonmail.ch>
2023-04-27 22:30:45 +0700

    add palindrome

--- PalindromeNumber/main.go
+++ PalindromeNumber/main.go
@@ -1,5 +1,50 @@
 package main
 
+import "fmt"
+
 func isPalindrome(x int) bool {
-	return false
+	comaparison := x
+	var res int = 0
+	var remainder int
+	for x > 0 {
+		remainder = x % 10
+		res = (res * 10) + remainder
+		x = x / 10
+	}
+	// 121
+	// 121 % 10 = 1
+	// hasil = (0 * 10) + 1 = 1
+	// x = 121 / 10 = 12.1
+
+	// 12
+	// 12 % 10 = 2
+	// hasil = (1 * 10 ) + 2 = 12
+	// x = 12 / 10 = 1
+
+	// 1
+	// 1 % 10 = 1
+	// hasil = (12 * 10) + 1 = 121
+	// x = 1 / 10 = 0
+
+	if res != comaparison {
+		return false
+	}
+	return true
+}
+
+func isWordPalindrome(x string) bool {
+	for i := 0; i < len(x); i++ {
+		j := len(x) - 1 - i
+		if x[i] != x[j] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	a := isPalindrome(121)
+	b := isWordPalindrome("aba")
+	fmt.Println(a)
+	fmt.Println(b)
 }
